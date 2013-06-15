a6245b14b9b4d19a3722d150d776af6b0112941b
Eivind Uggedal <eivind@uggedal.com>
2013-06-15 11:58:22 +0200

    Emit path relative to root.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var path = require('path');
 var Stream = require('stream');
 
 module.exports = function(root) {
@@ -8,7 +9,7 @@ module.exports = function(root) {
   fs.readdir(root, function (err, files) {
     // TODO: emit err
     files.forEach(function (file) {
-      s.emit('data', file);
+      s.emit('data', path.join(root, file));
       // TODO: recurse dirs
     });
   });
