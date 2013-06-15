178559041f0301c9f90438f029c43fecf6f9b9a7
Eivind Uggedal <eivind@uggedal.com>
2013-06-15 11:59:03 +0200

    Non-recursive test.

---
+++ test/flat.js
@@ -0,0 +1,21 @@
+var recurse = require('../');
+var test = require('tap').test;
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
+
+test('flat dir', function (t) {
+  mkdirp.sync('flat');
+  fs.openSync('flat/1.txt', 'w')
+  fs.openSync('flat/2.txt', 'w')
+
+  t.plan(2);
+
+  var flat = recurse('flat');
+  flat.on('data', function(data) {
+    t.ok(data.match(/flat\/\d\.txt/));
+  });
+
+  rimraf.sync('flat');
+
+});
