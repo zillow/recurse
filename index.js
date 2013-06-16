b165b2ed90ea4248a5cba24b03b25c53ed811803
Eivind Uggedal <eivind@uggedal.com>
2013-06-16 22:10:12 +0200

    Fix lstat error handling.

--- index.js
+++ index.js
@@ -2,33 +2,11 @@ var fs = require('fs');
 var path = require('path');
 var Stream = require('stream');
 
-function statdir(dir, names, cb) {
-  var entries = [];
-
-  if (!names.length) return cb(entries);
-
-  names.forEach(function (name) {
-    var relName = path.join(dir, name);
-
-    fs.lstat(relName, function (err, stats) {
-      if (err) s.emit('error', err);
-      entries.push({name: relName, isDir: stats && stats.isDirectory()});
-      if (entries.length == names.length) cb(entries);
-    });
-  });
-}
-
 module.exports = function (root) {
   var s = new Stream;
   s.readable = true
 
   var queue = [root];
-
-  function next() {
-    if (queue.length) return recurse(queue.pop());
-
-    s.emit('end');
-  }
 
   function recurse(dir) {
     fs.readdir(dir, function (err, names) {
@@ -48,6 +26,29 @@ module.exports = function (root) {
       });
     });
   }
+
+  function statdir(dir, names, cb) {
+    var entries = [];
+
+    if (!names.length) return cb(entries);
+
+    names.forEach(function (name) {
+      var relName = path.join(dir, name);
+
+      fs.lstat(relName, function (err, stats) {
+        if (err) s.emit('error', err);
+        entries.push({name: relName, isDir: stats && stats.isDirectory()});
+        if (entries.length == names.length) cb(entries);
+      });
+    });
+  }
+
+  function next() {
+    if (queue.length) return recurse(queue.pop());
+
+    s.emit('end');
+  }
+
 
   next();
 
