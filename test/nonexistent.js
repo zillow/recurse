fcaf76254110bece900dbd7db1802ae0eada79d5
Eivind Uggedal <eivind@uggedal.com>
2013-06-17 09:07:42 +0200

    Test for nonexistent dir.

---
+++ test/nonexistent.js
@@ -0,0 +1,12 @@
+var recurse = require('../');
+var test = require('tap').test;
+
+test('nonexiseant dir', function (t) {
+  t.plan(1);
+
+  var nonexistent = recurse('nonexiseant');
+
+  nonexistent.on('error', function (err) {
+    t.equal(err.code, 'ENOENT');
+  });
+});
