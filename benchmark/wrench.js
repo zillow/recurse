0ff460629b94ed665251f75e3ff9ffe86d6068ae
Eivind Uggedal <eivind@uggedal.com>
2013-06-19 00:32:38 +0200

    Add wrench to benchmarks.

---
+++ benchmark/wrench.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var wrench = require('wrench');
+
+var files = 0;
+wrench.readdirRecursive(process.env.HOME, function(err, curFiles) {
+  if (curFiles === null) {
+    console.log('wrench', files);
+  } else {
+    if (curFiles) files += curFiles.length; 
+  }
+});
