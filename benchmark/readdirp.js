0c02196673b5ab3014b4fc2bbc3fd25a08004208
Eivind Uggedal <eivind@uggedal.com>
2013-06-18 23:21:29 +0200

    Extract wc stream.

--- benchmark/readdirp.js
+++ benchmark/readdirp.js
@@ -1,17 +1,6 @@
 #!/usr/bin/env node
 
 var readdirp = require('readdirp');
-var Stream = require('stream');
+var wc = require('./wc');
 
-var dir = process.env.HOME;
-
-var lines = 0;
-var wc = new Stream;
-wc.writable = true;
-wc.write = function (chunk) {
-  lines++;
-}
-wc.end = function (chunk) {
-  console.log('readdirp', lines);
-}
-readdirp({ root: dir}).pipe(wc);
+readdirp({ root: process.env.HOME}).pipe(wc('readdirp'));
--- benchmark/recurse.js
+++ benchmark/recurse.js
@@ -1,17 +1,6 @@
 #!/usr/bin/env node
 
 var recurse = require('../');
-var Stream = require('stream');
+var wc = require('./wc');
 
-var dir = process.env.HOME;
-
-var lines = 0;
-var wc = new Stream;
-wc.writable = true;
-wc.write = function (chunk) {
-  lines++;
-}
-wc.end = function (chunk) {
-  console.log('recurse', lines);
-}
-recurse(dir).pipe(wc);
+recurse(process.env.HOME).pipe(wc('recurse'));
