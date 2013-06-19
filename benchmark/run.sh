70ecbce5e42cd2d55e6943215da81ce7bf11973f
Eivind Uggedal <eivind@uggedal.com>
2013-06-19 13:02:11 +0200

    Benchmark: note findit error.

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 bench() {
-  printf '%s: ' $1
-  time ./${1}.js
-  printf "\n"
+  printf '%s' $1
+  [ $# -eq 2 ] && printf ' (%s)' "$2"
+  printf ': '
+  time ./${1}.js 2>/dev/null
+  printf '\n'
 }
 
 bench find
 bench recurse
 bench wrench
 bench readdirp
-bench findit
+bench findit 'blows stack easily'
 bench walk
 bench dive
 bench match-files
