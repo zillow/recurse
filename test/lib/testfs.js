813cf74f1463f1741f97e0b2f3784cadcb49556d
Eivind Uggedal <eivind@uggedal.com>
2013-06-21 10:39:39 +0200

    Crude test file fixture function.

---
+++ test/lib/testfs.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+
+// TODO: extract this to a separate module.
+// TODO: pass errors to callback.
+
+module.exports = function (paths, prefix, cb) {
+  var dirs = [];
+  var files = [];
+  if (!prefix) prefix = '';
+
+  paths.forEach(function (p) {
+    // TODO: symlink support (-> separator).
+    if (p.substr(-1) === path.sep) {
+      dirs.push(path.join(prefix, p));
+    } else {
+      dirs.push(path.join(prefix, path.dirname(path)));
+      files.push(path.join(prefix, p));
+    }
+  });
+
+  // TODO: normalize dirs before creating them to save syscalls.
+
+  var pendingDirs = dirs.length;
+  dirs.forEach(function (dir) {
+    mkdirp(dir, function (err) {
+      pendingDirs--;
+      check();
+    });
+  });
+
+  var pendingFiles = files.length;
+  files.forEach(function (file) {
+    fs.open(file, 'w', function (err, fd) {
+      fs.close(fd, function (err) {
+        pendingFiles--;
+        check();
+      });
+    });
+  });
+
+  function check () {
+    if (!pendingDirs && !pendingFiles) cb(null);
+  }
+}
