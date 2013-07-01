1b13346bb6f9b6cb529dac2761f7c0be9eb93577
Eivind Uggedal <eivind@uggedal.com>
2013-07-01 10:14:22 +0200

    Use testfs for symlink tests.

--- test/resolvesymlinks.js
+++ test/resolvesymlinks.js
@@ -1,51 +1,47 @@
 var recurse = require('../');
 var test = require('tap').test;
 var fs = require('fs');
-var mkdirp = require('mkdirp');
-var rimraf = require('rimraf');
+var testfs = require('testfs');
 
-function createFixtures(d) {
-  rimraf.sync(d);
-  mkdirp.sync(d + '/target');
-  mkdirp.sync(d + '/links');
-  fs.openSync(d + '/target/1.txt', 'w');
-  fs.symlinkSync('../target', d + '/links/link');
-}
+var files = [
+  '/target/1.txt',
+  '../target -> /links/link'
+];
 
 test('resolving symlinks', function (t) {
   var d = 'resolvesymlinks';
-  createFixtures(d);
-
   t.plan(2);
 
-  var writes = 0;
+  var fs = testfs(d, files, function (err) {
+    var writes = 0;
 
-  var symlinks = recurse(d + '/links', {resolvesymlinks: true});
-  symlinks.on('data', function (data) {
-    t.equal(data, d + '/links/link/1.txt');
-    writes++;
-  });
-  symlinks.on('end', function () {
-    t.equal(writes, 1);
-    rimraf.sync(d);
+    var symlinks = recurse(d + '/links', {resolvesymlinks: true});
+    symlinks.on('data', function (data) {
+      t.equal(data, d + '/links/link/1.txt');
+      writes++;
+    });
+    symlinks.on('end', function () {
+      t.equal(writes, 1);
+      fs.rm();
+    });
   });
 });
 
 test('listing symlinks', function (t) {
-  var d = 'resolvesymlinks';
-  createFixtures(d);
-
+  var d = 'listymlinks';
   t.plan(2);
 
-  var writes = 0;
+  var fs = testfs(d, files, function (err) {
+    var writes = 0;
 
-  var symlinks = recurse(d + '/links');
-  symlinks.on('data', function (data) {
-    t.equal(data, d + '/links/link');
-    writes++;
-  });
-  symlinks.on('end', function () {
-    t.equal(writes, 1);
-    rimraf.sync(d);
+    var symlinks = recurse(d + '/links');
+    symlinks.on('data', function (data) {
+      t.equal(data, d + '/links/link');
+      writes++;
+    });
+    symlinks.on('end', function () {
+      t.equal(writes, 1);
+      fs.rm();
+    });
   });
 });
